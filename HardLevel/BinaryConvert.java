a5ab4fa08e3ecebf1232d22cfc767bd27278995c
riksanariyasedstem <riksana.riyas@edstem.com>
2024-11-27 15:45:10 +0530

    Create BinaryConvert.java

---
+++ HardLevel/BinaryConvert.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BinaryConvert {
+     public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         System.out.println("enter the binary code");
+         String bin = sc.nextLine();
+         String result = binaryConversion(bin);
+         System.out.println(result);
+
+     }
+    public static String binaryConversion(String bin) {
+        if(bin.length() > 8 || !bin.matches("[01]+")){
+            return "Not Valid ";
+        }
+        if(!bin.contains("0")||!bin.contains("1")){
+            return "Not valid ";
+        }
+        int tempInt = Integer.parseInt(bin,2);
+        char temchar =(char)tempInt ;
+        return Character.toString(temchar);
+
+    }
+}
