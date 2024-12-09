0c7ab36f04194439ced4825a08c3c3c41f372118
riksanariyasedstem <riksana.riyas@edstem.com>
2024-12-09 15:54:30 +0530

    Create  LibraryItem.java

---
+++ assisments/20241205/LibraryManagmentSystem/LibraryItem.java
@@ -0,0 +1,12 @@
+public abstract class LibraryItem {
+    String id;
+    String title;
+    boolean available = true;
+
+    public LibraryItem(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public abstract double calculateLateFee(int daysLate);
+}
