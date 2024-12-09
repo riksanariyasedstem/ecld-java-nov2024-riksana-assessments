d7e8ad93993f9727fc112b0ae2b43a71af331b97
riksanariyasedstem <riksana.riyas@edstem.com>
2024-12-09 15:58:28 +0530

    Create Order.java

---
+++ assisments/20241205/CustomerOrderSystem/Order.java
@@ -0,0 +1,20 @@
+import java.time.LocalDateTime;
+import java.util.List;
+
+public abstract class Order {
+    String orderId;
+    String customerName;
+    LocalDateTime orderDate;
+    List<OrderItem> items;
+
+    public Order(String orderId, String customerName, LocalDateTime orderDate, List<OrderItem> items) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.orderDate = orderDate;
+        this.items = items;
+    }
+
+
+    public abstract double calculateTotal();
+    public abstract double calculateDiscount();
+}
